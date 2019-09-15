063a42df3405134c83a7450e089b2d3c73c90899
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 01:21:53 +0200

    Buchstaben Auslese fertig

--- buchstaben.sh
+++ buchstaben.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-url=https://www.tierarzt-onlineverzeichnis.de/tieraerzte/deutschland/0/
+unset url \
+buchstaben \
+feldnummer \
+burl \
 
-curl $url
+
+# Statische Variablen
+url=https://www.tierarzt-onlineverzeichnis.de/tieraerzte/deutschland/0/
+buchstaben="A B C D E F G H I J K L M N O P Q R S T U V W X Z"
+feldnummer=2
+
+
+for buchstabe in $buchstaben;do
+	burl=$(curl -s $url | grep ">$buchstabe<" | cut -d "\"" -f$feldnummer)
+	echo "$buchstabe $burl"
+done
