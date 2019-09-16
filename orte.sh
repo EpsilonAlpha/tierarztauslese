4fc2baebed49f33b104295eac24cb320e1f71b9c
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 08:02:54 +0200

    Befehl erweitern

--- orte.sh
+++ orte.sh
@@ -10,4 +10,10 @@ url=$(cat $dateiname | head -n1 | cut -d" " -f2)
 
 curl -s $url | \
 grep bl_liste -A2 | \
-tail -n1
+tail -n1 | \
+sed -e 's/<\/li>/\n/g' \
+-e 's/<li>//g' \
+-e 's/<a href=//g' \
+-e 's/title=//g' \
+-e 's/>//g' \
+-e 's/<\/a//g'
