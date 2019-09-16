657891541bfe83d02f33472e2e7fba5366e4774b
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 09:47:46 +0200

    aerzte.sh erstellt und Datei-Erstellungstest

---
+++ aerzte.sh
@@ -0,0 +1,20 @@
+#!/bin/sh 
+
+# Variablen aufräumen
+unset buchstaben \
+buchstabe \
+ortsid \
+exportdatei
+
+# Statische Variablen
+buchstaben="A B C D E F G H I J K L M N O P Q R S T U V W X Z"
+
+for buchstabe in $buchstaben;do
+	for ortsid in $(cat "$buchstabe"_links.txt | cut -d" " -f1 | cut -d"/" -f6,7 | sed -e "s/\///g"),do
+		
+		# Datei-Name zusammenstellen
+		exportdatei="Buchstabe"$buchstabe"/"$ortsid"_aerzte.txt"
+
+		# Datei erstellen bzw. leeren
+		touch $exportdatei
+done
