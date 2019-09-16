1fc990620e086d5f83e82beeffca2a4c73d3ba53
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 07:28:04 +0200

    orte.sh hinzugefügt

---
+++ orte.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Variablen aufräumen
+unset url \
+dateiname
+
+# Statische Variablen
+dateiname=buchstaben_links.txt
+
+curl -s https://www.tierarzt-onlineverzeichnis.de/tieraerzte/deutschland/0/a/
