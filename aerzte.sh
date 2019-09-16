f521837746b88c20258bac2d264ccb8c170aa5bc
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 11:32:52 +0200

    aerzte.sh upgedatet

--- aerzte.sh
+++ aerzte.sh
@@ -10,11 +10,12 @@ exportdatei
 buchstaben="A B C D E F G H I J K L M N O P Q R S T U V W X Z"
 
 for buchstabe in $buchstaben;do
-	for ortsid in $(cat "$buchstabe"_links.txt | cut -d" " -f1 | cut -d"/" -f6,7 | sed -e "s/\///g"),do
+	for ortsid in $(cat "$buchstabe"_links.txt | cut -d" " -f1 | cut -d"/" -f6,7 | sed -e "s/\///g");do
 		
 		# Datei-Name zusammenstellen
 		exportdatei="Buchstabe"$buchstabe"/"$ortsid"_aerzte.txt"
 
 		# Datei erstellen bzw. leeren
 		touch $exportdatei
+	done
 done
