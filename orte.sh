1632a4dfec119df49b42143db7e149f4ef10a405
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 08:51:16 +0200

    orte.sh testen ob Dateien erstellt werden

--- orte.sh
+++ orte.sh
@@ -13,13 +13,17 @@ buchstaben="A B C D E F G H I J K L M N O P Q R S T U V W X Z"
 
 # Orte auslesen und in individuellen Dateien ablegen
 for buchstabe in $buchstaben;do
-    # Link für den Buchstaben aus der Import-Datei holen
-    url=$(cat $importdatei | grep $buchstabe | cut -d" " -f2)
+	# Link für den Buchstaben aus der Import-Datei holen
+	url=$(cat $importdatei | grep $buchstabe | cut -d" " -f2)
 
-    # Export-Datei definieren
-    exportdatei=$buchstabe_links.txt
-    # Datei leeren
-    touch $exportdatei
-
-    curl -s $url | grep bl_liste -A2 | tail -n1 | sed -e 's/<\/li>/\n/g' -e 's/<li>//g' -e 's/<a href=//g' -e 's/title=//g' -e 's/>//g' -e 's/<\/a//g' | cut -d"'" -f-4 | sed -e "s/'//g" >> $exportdatei
+	# Export-Datei definieren
+	exportdatei=$buchstabe_links.txt
+	# Datei leeren
+	touch $exportdatei
+	if [ -f $exportdatei ];then
+		echo "Datei $exportdatei vorhanden"
+	else
+		echo "Datei $exportdatei nicht vorhanden"
+	fi
+	#curl -s $url | grep bl_liste -A2 | tail -n1 | sed -e 's/<\/li>/\n/g' -e 's/<li>//g' -e 's/<a href=//g' -e 's/title=//g' -e 's/>//g' -e 's/<\/a//g' | cut -d"'" -f-4 | sed -e "s/'//g" >> $exportdatei
 done
