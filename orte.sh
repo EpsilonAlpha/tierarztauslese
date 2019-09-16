cbb797c688fc9c92879e482b8de1c5373e41d3c5
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 08:42:41 +0200

    orte.sh Zeilenumbrüche raus

--- orte.sh
+++ orte.sh
@@ -21,15 +21,5 @@ for buchstabe in $buchstaben;do
     # Datei leeren
     touch $exportdatei
 
-    curl -s $url | \
-    grep bl_liste -A2 | \
-    tail -n1 | \
-    sed -e 's/<\/li>/\n/g' \
-    -e 's/<li>//g' \
-    -e 's/<a href=//g' \
-    -e 's/title=//g' \
-    -e 's/>//g' \
-    -e 's/<\/a//g' | \
-    cut -d"'" -f-4 | \
-    sed -e "s/'//g" >> $exportdatei
+    curl -s $url | grep bl_liste -A2 | tail -n1 | sed -e 's/<\/li>/\n/g' -e 's/<li>//g' -e 's/<a href=//g' -e 's/title=//g' -e 's/>//g' -e 's/<\/a//g' | cut -d"'" -f-4 | sed -e "s/'//g" >> $exportdatei
 done
