d9972e9a0523505efff7e510125367df4bb9a2bc
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 08:29:12 +0200

    orte.sh Schleife für alle Buchstaben

--- orte.sh
+++ orte.sh
@@ -2,20 +2,26 @@
 
 # Variablen aufräumen
 unset url \
-dateiname
+importdatei \
+buchstaben
 
 # Statische Variablen
-dateiname=buchstaben_links.txt
-url=$(cat $dateiname | head -n1 | cut -d" " -f2)
+importdatei=buchstaben_links.txt
+buchstaben="A B C D E F G H I J K L M N O P Q R S T U V W X Z"
 
-curl -s $url | \
-grep bl_liste -A2 | \
-tail -n1 | \
-sed -e 's/<\/li>/\n/g' \
--e 's/<li>//g' \
--e 's/<a href=//g' \
--e 's/title=//g' \
--e 's/>//g' \
--e 's/<\/a//g' | \
-cut -d"'" -f-4 | \
-sed -e "s/'//g"
+
+for buchstabe in $buchstaben;do
+    url=$(cat $importdatei | grep $buchstabe | cut -d" " -f2)
+
+    curl -s $url | \
+    grep bl_liste -A2 | \
+    tail -n1 | \
+    sed -e 's/<\/li>/\n/g' \
+    -e 's/<li>//g' \
+    -e 's/<a href=//g' \
+    -e 's/title=//g' \
+    -e 's/>//g' \
+    -e 's/<\/a//g' | \
+    cut -d"'" -f-4 | \
+    sed -e "s/'//g"
+done
