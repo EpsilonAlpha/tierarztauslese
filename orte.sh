8493944d286fc04f921b9d94acd6bde00ff836ac
EpsilonAlpha <flirtmaster@me.com>
2019-09-16 08:54:03 +0200

    orte.sh Curl dem Test hinzufügen

--- orte.sh
+++ orte.sh
@@ -22,8 +22,8 @@ for buchstabe in $buchstaben;do
 	touch $exportdatei
 	if [ -f $exportdatei ];then
 		echo "Datei $exportdatei vorhanden"
+		curl -s $url | grep bl_liste -A2 | tail -n1 | sed -e 's/<\/li>/\n/g' -e 's/<li>//g' -e 's/<a href=//g' -e 's/title=//g' -e 's/>//g' -e 's/<\/a//g' | cut -d"'" -f-4 | sed -e "s/'//g" >> $exportdatei
 	else
 		echo "Datei $exportdatei nicht vorhanden"
 	fi
-	#curl -s $url | grep bl_liste -A2 | tail -n1 | sed -e 's/<\/li>/\n/g' -e 's/<li>//g' -e 's/<a href=//g' -e 's/title=//g' -e 's/>//g' -e 's/<\/a//g' | cut -d"'" -f-4 | sed -e "s/'//g" >> $exportdatei
 done
