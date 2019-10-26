f25df15ae0bcb211ad0d7c06555f8683beb84421
EpsilonAlpha <flirtmaster@me.com>
2019-10-26 12:53:54 +0200

    Update aerzte.sh

--- aerzte.sh
+++ aerzte.sh
@@ -16,7 +16,7 @@ rm $fehlerlinks
 
 for buchstabe in $buchstaben;do
 		for link in $(cat "$buchstabe"_links.txt );do
-			if [[ "$link" == '' ]]; then 
+			if [[ "$link" == "" ]]; then 
 				# ortsid ermitteln für den Dateinamen hernehmen
 				ortsid=$(echo "$link" \
 				| tr -s ' ' \
