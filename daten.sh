bd48fe66ff089f828992292c85b25df58ee48b78
EpsilonAlpha <flirtmaster@me.com>
2019-10-26 15:25:58 +0200

    Update daten.sh

--- daten.sh
+++ daten.sh
@@ -2,7 +2,11 @@
 
 # Variablen aufräumen
 unset buchstaben \
-buchstabe
+buchstabe \
+modus
+
+# Übergabe-Variablen
+modus=$1
 
 # Statische Variablen
 buchstaben="A B C D E F G H I J K L M N O P Q R S T U V W X Z"
