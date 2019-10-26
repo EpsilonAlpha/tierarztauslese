d2f7dc8193a9c59ecf252488b0032651f235fc00
EpsilonAlpha <flirtmaster@me.com>
2019-10-26 15:24:47 +0200

    Update daten.sh

--- daten.sh
+++ daten.sh
@@ -52,9 +52,9 @@ einzeln() {
 
 # Auswahl des Modus
 case $modus in 
-  * )
-    alles
-  ;;
+  #* )
+  #  alles
+  #;;
   A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Z )
     einzeln $modus
   ;;
