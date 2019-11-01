98a4156ce076f4c0b1bc44ec2724e393bc8f359f
EpsilonAlpha <flirtmaster@me.com>
2019-11-01 12:15:55 +0100

    Quoting beim curl Befehl von " auf ' umgeändert

--- daten.sh
+++ daten.sh
@@ -28,10 +28,10 @@ einzeln() {
     # jeden Link, einen nach dem Anderen abarbeiten
     for link in $(cat $workdir/"$buchstabe"_links.txt | cut -d" " -f1 | grep -e '^http.*$' | head -1 );do
       # Ausgabe des Links zur Diagnose
-      echo "Link ist $link"
+      echo "Link ist $link";
       
       ausgabe=$(curl -s $link |\
-      ausgabe=$(echo $ausgabe | sed -n "/<div class=\"do_adresse\">/,/<\/div>/p")
+      ausgabe=$(echo $ausgabe | sed -n '/<div class=\"do_adresse\">/,/<\/div>/p');
       ausgabe=$(echo $ausgabe | tr -s ' ')
       ausgabe=$(echo $ausgabe | tr -d '\t')
       ausgabe=$(echo $ausgabe | cut -d">" -f2)
